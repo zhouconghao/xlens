056f183ee862490bf02a81170a00384fca2ba3f6
Zhou Conghao <zchusre@gmail.com>
2024-08-02 16:52:23 -0700

    add mdet class

--- xlens/simulation/measure/__init__.py
+++ xlens/simulation/measure/__init__.py
@@ -1,10 +1,12 @@
 from . import utils
 from .base import DMMeasurementTask, ProcessSimDM
 from .fpfs import ProcessSimFpfs
+from .metadetect import ProcessSimMetadetect
 
 __all__ = [
     "ProcessSimDM",
     "ProcessSimFpfs",
+    "ProcessSimMetadetect"
     "utils",
     "DMMeasurementTask",
 ]
